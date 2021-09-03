cfd114c1ec058a967daf25b3fcb6375886811376
Jack Liu <jalliu@cs.stonybrook.edu>
2021-09-03 16:54:48 -0400

    Implemented validargs

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -16,14 +16,58 @@
 #error "Do not #include <ctype.h>. You will get a ZERO."
 #endif
 
+/*
+
+Parts 2-5:
+LOL JUST DO THEM
+
+Pre-submission:
+Run through every function one by one for test cases and run program for test cases
+There should be no additional prints to console or anything other than needed(USAGE)
+
+to be safe this is a joke and I won't print lol
+In an unsuccessful run in which the program exits with EXIT_FAILURE it is permissible to output to stderr a diagnostic message that indicates the reason for the failure, even if the program has not been compiled for debugging.
+*/
 int main(int argc, char **argv)
 {
-    if(validargs(argc, argv))
-        USAGE(*argv, EXIT_FAILURE);
-    if(global_options & HELP_OPTION)
-        USAGE(*argv, EXIT_SUCCESS);
+    if(validargs(argc, argv)){ //If the argument was invalid(-1/non-zero/TRUE), we want to show the usage
+
+debug("global_options:%x\n",global_options); //print the hex of global_options
+
+        USAGE(*argv, EXIT_FAILURE); //We will show the user the correct flags and return with exit failure
+    } //The arguments must be valid and global_option will be set as appropiate
+    if(global_options & HELP_OPTION){ //If the help option flag is enabled, we want to show the usage
+
+debug("global_options:%x\n",global_options); //print the hex of global_options
+
+        USAGE(*argv, EXIT_SUCCESS); //Since this is intended, we will return with exit success
+    } //Any garbage values for the first 3 flag bits will have not reset since args are valid
+    if(global_options & ENCODE_OPTION){ //If the encode option flag is enabled, we want to run the function
+        debug("AHHHHHHHH ENCODE ME\n");
+    /*
+    If the -e flag is provided, then the program will read uncompressed data from
+    standard input (stdin) and emit compressed data to standard output (stdout)
+        tldr run encode function in if statement
+        if we get a -1(TRUE), we do a goofy looking exit(exit-failure)
+        if we get a 0(false), we do a goofy exit successs kinda guy
+    */
+
+debug("global_options:%x\n",global_options); //print the hex of global_options
+
+    } //Any garbage values for the first 3 flag bits will have not reset since args are valid
+    if(global_options & DECODE_OPTION){ //If the decode option flag is enabled, we want to run the function
+        debug("AHHHHHHHH DECODE ME\n");
+    /*
+    If the -d flag is provided, then the program will read compressed data from
+    standard input (stdin) and emit uncompressed data to standard output (stdout).
+    */
+
+debug("global_options:%x\n",global_options); //print the hex of global_options
+
+    } //Any garbage values for the first 3 flag bits will have not reset since args are valid
+
     // TO BE IMPLEMENTED
-    return EXIT_FAILURE;
+    return EXIT_FAILURE; //This implies that the encode/decode functions ran into an error
 }
 
 /*
--- hw1/src/mtft.c
+++ hw1/src/mtft.c
@@ -8,6 +8,9 @@
 #include "mtft.h"
 #include "debug.h"
 
+static int strEquals(char *str1, char *str2); //compares two strings
+
+
 /**
  * @brief  Given a symbol value, determine its encoding (i.e. its current rank).
  * @details  Given a symbol value, determine its encoding (i.e. its current rank)
@@ -138,7 +141,7 @@ int mtf_encode() {
  *
  * Any 0 bits that occur as padding after the last code word are discarded
  * and do not contribute to the output.
- * 
+ *
  * Note that (as for mtf_encode()) the transformation performed by this
  * function is to be done in an "online" fashion: the entire input need not
  * (and should not) be read before output is produced.
@@ -169,6 +172,71 @@ int mtf_decode() {
  */
 
 int validargs(int argc, char **argv) {
-    // TO BE IMPLEMENTED
-    return -1;
+    int global_options_save = global_options; //Save the value of global_options if it's not 0x0(piazza)
+    global_options <<= 3; //If there isn't, then it's just a few more lines of code
+    global_options = (unsigned)global_options>>3; //Cast it to be unsigned so we get logical shift right
+    if(argc==1){ //We want to make sure that there is a flag provided
+        return -1; //The only argument passed was the filename
+    } //Note: 0 args implies no -h AND none of -e/-d(none!=exactly one)
+    argv++; //Skips file name(zeroth argument). I didn't use switch/case since 4 cases doesn't speed it up
+    if(strEquals((*argv), "-h")){ //Check if the help flag is enabled. Could have ++(*argv) to save a line
+        global_options |= HELP_OPTION; //Do bitwise OR on global_options to set the bit
+        return 0; //We don't want to check any other arguments if -h is provided
+    } //From this point on, the -h flag should not appeart(it must the first flag)
+    if(strEquals((*argv), "-e")){ //Check if the encode flag is enabled
+        global_options |= ENCODE_OPTION; //Do bitwise OR on global_options to set the bit
+    }else if(strEquals(*argv, "-d")){ //Check if the decode flag is enabled
+        global_options |= DECODE_OPTION; //Do bitwise OR on global_options to set the bit
+    } else { //The byte flag cannot occur out of order, and any other options are invalid
+        return -1; //The first flag isn't valid and global_options hasn't been changed yet
+    } //From this point on, the only flag allowed is -b(-e/-d cannot exist with each other)
+    if(argc==2){ //We only had the first flag, so we are using the default value of 1 for the byte
+        global_options >>= 7; //Erase the 7 LSBs, the new MSBs can be ignored since we will shift left
+        global_options <<= 7; //Re-introduce the LSBs but as 0 due to the logical left shift
+        global_options |= 0x1; //The -b flag could not have existed so the byte value is 1
+        return 0; //I tried to make the setting bytes as a macro, but it failed and this isn't overused
+    } //From this point on, we are reading the -b flag
+    argv++; //We want to check the second flag//third argument(should always be the byte flag)
+    if(!strEquals((*argv), "-b")){ //We have to check that is actually is the -b flag
+        global_options = global_options_save; //Restore the value if there is an error
+        return -1; //The flag was invalid(-h returns, -e/-d cannot coexist and other values don't match)
+    }//We now know that the flag must be -b
+    if(argc!=4){ //We should have 4 arguments at this point: filename, -h/-e/-d, -b, byte specification
+        global_options = global_options_save; //Restore the value if there is an error
+        return -1; //If that isn't the case, the number of arguments is invalid
+    } //It may be optimal to check this before checking for -b, but I think it depends on test cases
+    argv++; //We want to check the parameter for the byte flag
+    while((**argv)=='0'){ //We want to accept any amount of leading zeros
+        (*argv)++; //Read the next character until we are out of leading zeros
+    } //This means that the specification in the byte flag should have no more 0(only accept 1/2 now)
+    switch((**argv)){ //I decided I should learn how to use it and I didn't want to refactor the code...
+        case '1': //We want to set the last 7 bytes to 0x1
+            global_options >>= 7; //Erase the 7 LSBs, the new MSBs can be ignored since we will shift left
+            global_options <<= 7; //Re-introduce the LSBs but as 0 due to the logical left shift
+            global_options |= 0x1; //The -b flag could not have existed so the byte value is 1
+            break; //We want to make sure there are no values after here otherwise 10 could pass
+        case '2': //We want to set the last 7 bytes to 0x2
+            global_options >>= 7; //Erase the 7 LSBs, the new MSBs can be ignored since we will shift left
+            global_options <<= 7; //Re-introduce the LSBs but as 0 due to the logical left shift
+            global_options |= 0x2; //The -b flag could not have existed so the byte value is 2
+            break; //We want to make sure there are no values after here otherwise 20 could pass
+        default: //The argument must have been invalid
+            global_options = global_options_save; //Restore the value if there is an error
+            return -1; //Therefore, we should return invalid arguments
+    } //From here, we want to check that there are no more characters
+    (*argv)++; //This character should be '\0' or the null character
+    if((**argv)!='\0'){ //If it wasn't null, we should return an error
+        global_options = global_options_save; //Restore the value if there is an error
+        return -1; //The artgument was invalid
+    } //We couldn't use the same return style as strEquals since it would not return 0/-1 without hassle
+    return 0; //All cases of invalid arguments were dealt with prior to this point
 }
+
+static int strEquals(char *str1, char *str2) { //Compare two strings and returns true if they were equal
+    for(; (*str1)!='\0'; str1++,str2++){ //Iterate through str1 until we reach the null/end
+        if((*str1)!=*(str2)){ //Compare the current characters in both string
+            return 0; //The characters were not equal so the string must not be
+        } //The current chracters of each string were equal, so we want to move on
+    } //This loop catches all cases ONLY when str1 is shorter than str2
+    return (*str2)=='\0'; //If str2 is longer/did not reach the end, it must not be the same string
+} //Found some ways to (maybe) make the code more efficient(and harder to read but comments I guess)
