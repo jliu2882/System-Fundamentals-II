7703e1a01666dbbf40771d2a04fdaba8abecf1a3
Jack Liu <jalliu@cs.stonybrook.edu>
2021-09-10 16:25:37 -0400

    Started implementation of mtf_map_encode

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -17,33 +17,26 @@
 #endif
 
 /*
+@some kinda of oh:
+ask if strEQuals(argv, "-h") is ok or is ther garabage
+maybe how to bild tree
 
-Parts 2-5:
-LOL JUST DO THEM
-
-Pre-submission:
 Run through every function one by one for test cases and run program for test cases
 There should be no additional prints to console or anything other than needed(USAGE)
-
-to be safe this is a joke and I won't print lol
-In an unsuccessful run in which the program exits with EXIT_FAILURE it is permissible to output to stderr a diagnostic message that indicates the reason for the failure, even if the program has not been compiled for debugging.
 */
 int main(int argc, char **argv)
 {
     if(validargs(argc, argv)){ //If the argument was invalid(-1/non-zero/TRUE), we want to show the usage
-
-debug("global_options:%x\n",global_options); //print the hex of global_options
-
         USAGE(*argv, EXIT_FAILURE); //We will show the user the correct flags and return with exit failure
     } //The arguments must be valid and global_option will be set as appropiate
     if(global_options & HELP_OPTION){ //If the help option flag is enabled, we want to show the usage
-
-debug("global_options:%x\n",global_options); //print the hex of global_options
-
         USAGE(*argv, EXIT_SUCCESS); //Since this is intended, we will return with exit success
-    } //Any garbage values for the first 3 flag bits will have not reset since args are valid
+    } //Any garbage values for the first 3 flag bits will have not been reset since args are valid
     if(global_options & ENCODE_OPTION){ //If the encode option flag is enabled, we want to run the function
-        debug("AHHHHHHHH ENCODE ME\n");
+        mtf_encode();
+
+
+
     /*
     If the -e flag is provided, then the program will read uncompressed data from
     standard input (stdin) and emit compressed data to standard output (stdout)
@@ -51,20 +44,14 @@ debug("global_options:%x\n",global_options); //print the hex of global_options
         if we get a -1(TRUE), we do a goofy looking exit(exit-failure)
         if we get a 0(false), we do a goofy exit successs kinda guy
     */
-
-debug("global_options:%x\n",global_options); //print the hex of global_options
-
-    } //Any garbage values for the first 3 flag bits will have not reset since args are valid
+    } //Any garbage values for the first 3 flag bits will have not been reset since args are valid
     if(global_options & DECODE_OPTION){ //If the decode option flag is enabled, we want to run the function
         debug("AHHHHHHHH DECODE ME\n");
     /*
     If the -d flag is provided, then the program will read compressed data from
     standard input (stdin) and emit uncompressed data to standard output (stdout).
     */
-
-debug("global_options:%x\n",global_options); //print the hex of global_options
-
-    } //Any garbage values for the first 3 flag bits will have not reset since args are valid
+    } //Any garbage values for the first 3 flag bits will have not been reset since args are valid
 
     // TO BE IMPLEMENTED
     return EXIT_FAILURE; //This implies that the encode/decode functions ran into an error
--- hw1/src/mtft.c
+++ hw1/src/mtft.c
@@ -8,12 +8,40 @@
 #include "mtft.h"
 #include "debug.h"
 
-static int strEquals(char *str1, char *str2); //compares two strings
+static int depth; //Keep track of the depth of the tree which is also how many bits the offset should be
+static int powerOfTwo; //I chose this rather than a lg/ceil approach since I'm declaring a variable for depth
+static int strEquals(char *str1, char *str2) { //Compare two strings and returns true if they were equal
+    for(; (*str1)!='\0'; str1++,str2++){ //Iterate through str1 until we reach the null/end
+        if((*str1)!=*(str2)){ //Compare the current characters in both string
+            return 0; //The characters were not equal so the string must not be
+        } //The current chracters of each string were equal, so we want to move on
+    } //This loop catches all cases ONLY when str1 is shorter than str2
+    return (*str2)=='\0'; //If str2 is longer/did not reach the end, it must not be the same string
+} //Found some ways to (maybe) make the code more efficient(and harder to read but comments I guess)
+
+
+//DELETE AND FIX
+static SYMBOL getSymbol() { //Return the number of bytes that a symbol represents
+    if(global_options & 1){ //The arguments must be valid and 0x1(01) would not match with 0x2(10)
+        return getchar(); //Read one character and return it
+    } else { //If the flag wasn't 1, then it must be 2 since there are only 2 valid arguments
+        //return getchar()<<8 | getchar(); //Is cool but doesn't allow for checking each individual byte
+        SYMBOL byte1 = getchar(); //Read the first byte
+        if(byte1==-1) //We don't want to return if the byte is valid
+            return byte1; // If the first character is -1, then we want to return -1
+        SYMBOL byte2 = getchar(); //Read the second byte
+        if(byte2==-1) //This should return an error since we have an odd number of input bytes
+            return -1;
+    } //Does not treat -1/end of file any differently than normal bytes
+    return -1;
+} //NOTE THS SHOUKLD BE DELELTED AND REDONE IN MTFENCODE
+//while i have me here, initalize the values of last_offset to be NO_OFFSET
+
 
 
 /**
  * @brief  Given a symbol value, determine its encoding (i.e. its current rank).
- * @details  Given a symbol value, determine its encoding (i.e. its current rank)
+ * @details  Given a symbol va`lue, determine its encoding (i.e. its current rank)
  * according to the "move-to-front" transform described in the assignment
  * handout, and update the state of the "move-to-front" data structures
  * appropriately.
@@ -26,7 +54,7 @@ static int strEquals(char *str1, char *str2); //compares two strings
  * (depending on the value of the BYTES field in the global_options variable),
  * which is the rank currently assigned to the argument symbol.
  * The first time a symbol is encountered, it is assigned
- * a default rank computed by adding 512 or 131072 to its value.
+ * a default rank computed by adding 256 or 65536 to its value.
  * A symbol that has already been encountered is assigned a rank in the
  * range [0, 255], [0, 65535], according to how recently it has occurred
  * in the input compared to other symbols that have already been seen.
@@ -38,9 +66,75 @@ static int strEquals(char *str1, char *str2); //compares two strings
  * @modifies  The state of the "move-to-front" data structures.
  */
 CODE mtf_map_encode(SYMBOL sym) {
-    // TO BE IMPLEMENTED.
-    return NO_SYMBOL;
-}
+    // TO BE IMPLEMENTED. useless comment but looks nicer for now :)
+debug("PRE SYMBOL:%c/%u",sym,sym);
+debug("PRE LAST OFFSET:%li",*(last_offset+sym));
+debug("PRE CURRENT OFFSET:%lu",current_offset);
+//when current_offset = power_of_2 then power_of_two multiply by 2 and depth++
+    //creat new parent node
+
+    CODE rank; //I think CODE is the proper typedef for rank but honestly as long as it's int, it's good
+    if(*(last_offset+sym)==NO_OFFSET){ //We have not seen this symbol yet so we have to add an offset
+        if(global_options & 1){ //The arguments must be valid and 0x1(01) would not match with 0x2(10)
+            rank = sym+256; //We add 256 since that can unmistakably be deciphered as sym
+        } else{ //If the flag wasn't 1, then it must be 2 since there are only 2 valid arguments
+            rank = sym+65536; //To elaborate on why we add, the first 65536 are for "seen before rank"
+        } //We have gotten the rank of the symbol
+    } else{ //We found the offset for the last time we used the symbol
+
+
+        /* *(last_offset+sym) = offset for symbol
+         *descend the tree to reach the leaf node with 0-left/1-right from MSB->LSB
+         *now we want to determine rank
+         *  ascend the tree by going to parent node
+         *  delete any nodes that become leaf
+         *  Also remember to keep the values of the tree consistent
+         *  Use right_count to determine rank
+         *      On the parent node, check left/right child
+         *           If we came from left, add right_count
+         *           If we came from right, add 0
+         *              AKA: keep address of current, go to parent and compare with left/right child
+         *HOW TO BUILD TREE ACCORDING TO TINY BRAIN - idk lol
+
+    typedef struct mtf_node {
+        struct mtf_node *left_child;   // Pointer to left child node
+        struct mtf_node *right_child;  // Pointer to right child node
+        struct mtf_node *parent;       // Pointer to parent node
+        int left_count;                // Number of symbols stored below left child
+        int right_count;               // Number of symbols stored below right child
+        SYMBOL symbol;                 // Symbol stored at this node (only at a leaf)
+    } MTF_NODE;
+
+         */
+
+
+    } //We got the rank of the symbol and removed the node it was last seen at
+
+
+    //start work here
+    //create an if/else for if tree exists
+        //if(depth == 0) //aka if tree is just chillig with itself
+            //set the node symbol to first bit
+        //otherwise, we can do the 0/1 trock
+
+    /*Descend the tree using our 0/1 trick
+     *   If we find any nodes that point to null, we want to initialize the node
+     *       Find a node from recycling (or the main pool if none exist)
+     *   On the final node(aka the leaf), set the symbol to be sym
+     *   Also remember to keep the values of the tree consistent
+     */
+    *(last_offset+sym) = current_offset; //The place our symbol can be found is at the current_offset
+    current_offset++; //Move onto the next offset so we can insert more symbols
+
+debug("RANK:%u", rank);
+debug("POST SYMBOL:%c/%u",sym,sym);
+debug("POST LAST OFFSET:%li",*(last_offset+sym));
+debug("POST CURRENT OFFSET:%lu",current_offset);
+
+
+
+    return rank; //Returns the rank of the symbol sym
+} //return NO_SYMBOL; //default return seems to bewrong to me though
 
 /**
  * @brief Given an integer code, return the symbol currently having that code.
@@ -112,7 +206,28 @@ SYMBOL mtf_map_decode(CODE code) {
  * @return 0 if the operation completes without error, -1 otherwise.
  */
 int mtf_encode() {
-    // TO BE IMPLEMENTED
+    for(int i=0; i<SYMBOL_MAX; i++){ //We want to loop through for every value in last_offset
+        *(last_offset+i)=NO_OFFSET; //Get the value at each index and set it to a default NO_OFFSET
+    }
+    first_unused_node_index = current_offset = depth = 0; //Just paranoid, I guess
+    powerOfTwo = 1; //when current_offset reaches 1(and every subsequent powOf2), we will increase the depth
+
+    //initialize a node for MTF_NODE *mtf_map, so first symbol(0 offset) doesn't increase depth
+        //next is offset 1(pow2) which will make depth 1(parent with 1 depth child)
+        //next if offset 2(pow2) which will make depth 2(paren with 2 depth childd)
+        //next if offset 4(pow2) will make deph 3(offset3 is not pow2 so 2deph good (0,1,2,3))
+            //pull from MTF_NODE node_pool[MAX_NODES]
+                //"probably &node_pool[0]"
+                //akak &(*(node_pool+0))
+
+    //MTF_NODE *recycled_node_list  dont' need intializing
+
+
+
+    mtf_map_encode(getchar());
+debug("\n\n\n");
+    mtf_map_encode(getchar());
+
     return -1;
 }
 
@@ -173,7 +288,7 @@ int mtf_decode() {
 
 int validargs(int argc, char **argv) {
     int global_options_save = global_options; //Save the value of global_options if it's not 0x0(piazza)
-    global_options <<= 3; //If there isn't, then it's just a few more lines of code
+    global_options <<= 3; //If there isn't garbage, then it's just a some lines of code/allocated memory
     global_options = (unsigned)global_options>>3; //Cast it to be unsigned so we get logical shift right
     if(argc==1){ //We want to make sure that there is a flag provided
         return -1; //The only argument passed was the filename
@@ -231,12 +346,3 @@ int validargs(int argc, char **argv) {
     } //We couldn't use the same return style as strEquals since it would not return 0/-1 without hassle
     return 0; //All cases of invalid arguments were dealt with prior to this point
 }
-
-static int strEquals(char *str1, char *str2) { //Compare two strings and returns true if they were equal
-    for(; (*str1)!='\0'; str1++,str2++){ //Iterate through str1 until we reach the null/end
-        if((*str1)!=*(str2)){ //Compare the current characters in both string
-            return 0; //The characters were not equal so the string must not be
-        } //The current chracters of each string were equal, so we want to move on
-    } //This loop catches all cases ONLY when str1 is shorter than str2
-    return (*str2)=='\0'; //If str2 is longer/did not reach the end, it must not be the same string
-} //Found some ways to (maybe) make the code more efficient(and harder to read but comments I guess)
